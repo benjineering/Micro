d4725099c891580bda5f1e629046eee5309e3b3c
Ben Williams <8enwilliams@gmail.com>
2025-04-28 15:31:30 +1000

    lambda kind working

--- Micro.Dev.Cdk/src/MicroDevCdk/MicroDevCdkStack.cs
+++ Micro.Dev.Cdk/src/MicroDevCdk/MicroDevCdkStack.cs
@@ -8,9 +8,15 @@ namespace MicroDevCdk
     {
         internal MicroDevCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            _ = new Function(scope, "MicroDevFunction", new FunctionProps
+            _ = new Function(this, "MicroDevFunction", new FunctionProps
             {
-
+                Runtime = Runtime.PROVIDED_AL2023,
+                Handler = "bootstrap",
+                Code = Code.FromAsset(@"C:\Users\8enwi\OneDrive\Desktop\bootstrap.zip"),
+                Architecture = Architecture.ARM_64,
+                MemorySize = 128,
+                Timeout = Duration.Seconds(10),
+                Description = ".NET 9 AOT Lambda on Graviton4",
             });
         }
     }
